69b3b4515465db531e2b943822a16cdeb12c6647
codyThompson <cody.thompson22@gmail.com>
2015-06-25 22:41:59 -0700

    TileDrawablePack - added TileDrawablePack

---
+++ flatsim/TileDrawablePack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace flatsim
+{
+    public abstract class TileDrawablePack
+    {
+        public abstract void drawStructure(TilePerspective.TileDrawInfo drawInfo);
+        public abstract void drawSurface(TilePerspective.TileDrawInfo drawInfo);
+        public abstract void drawLeftFace(TilePerspective.TileDrawInfo drawInfo);
+        public abstract void drawRightFace(TilePerspective.TileDrawInfo drawInfo);
+
+        public virtual void draw(TilePerspective.TileDrawInfo drawInfo, TilePart part)
+        {
+            switch (part)
+            {
+                case TilePart.STRUCTURE:
+                    drawStructure(drawInfo);
+                    break;
+                case TilePart.SURFACE:
+                    drawSurface(drawInfo);
+                    break;
+                case TilePart.LEFTFACE:
+                    drawLeftFace(drawInfo);
+                    break;
+                case TilePart.RIGHTFACE:
+                    drawRightFace(drawInfo);
+                    break;
+            }
+        }
+    }
+}
