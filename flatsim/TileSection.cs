9822b52945c632bd92d3da0ad479099a41ab5eaf
codyThompson <cody.thompson22@gmail.com>
2015-07-29 20:50:07 -0700

    Created tile section that acts as a part of possibly multiple tile sections per tile.

---
+++ flatsim/TileSection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace flatsim
+{
+    public class TileSection
+    {
+        float minHeight = 0;
+        float maxHeight = 0;
+       
+        TileDrawablePack drawablePack;
+
+        public TileSection(TileDrawablePack drawablePack)
+        {
+            this.drawablePack = drawablePack;
+        }
+
+        public void update()
+        {
+            // TODO update drawablePack
+        }
+
+        public void draw(int coordNS, int coordWE, TilePart tilePart, TilePerspective perspective, SpriteBatch spriteBatch)
+        {
+            float[] heights = drawablePack.getHeightRange(tilePart, minHeight, maxHeight);
+            TilePerspective.TileDrawInfo tdi;
+            foreach (float height in heights)
+            {
+                tdi = perspective.getTileDrawInfo(coordNS, coordWE, height, tilePart);
+                drawablePack.draw(tdi, spriteBatch);
+            }
+        }
+    }
+}
