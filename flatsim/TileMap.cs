be2bbc1b94229e960a0d54c89ab58ec178a0318b
codyThompson <cody.thompson22@gmail.com>
2015-08-06 21:27:59 -0700

    Created a TileMap class that aggregates Tiles

---
+++ flatsim/TileMap.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace flatsim
+{
+    public class TileMap
+    {
+        public TilePerspective perspective;
+
+        /*
+         *     /\   <--
+         *    /\/\    |
+         * W /\/\/\ N
+         *  /\/\/\/\
+         * /\/\/\/\/\
+         * \/\/\/\/\/
+         *  \/\/\/\/
+         * S \/\/\/ E
+         *    \/\/
+         *     \/
+         */
+        Tile[,] tiles;
+
+        public TileMap(int tilesNS, int tilesWE)
+        {
+            tiles = new Tile[tilesNS, tilesWE];
+            for (int ns = 0; ns < tilesNS; ns++)
+            {
+                for (int we = 0; we < tilesWE; we++)
+                {
+                    tiles[ns, we] = new Tile(ns, we);
+                }
+            }
+        }
+
+        public virtual void update(int elapsedMillis)
+        {
+            for (int ns = 0; ns < tiles.GetLength(0); ns++)
+            {
+                for (int we = 0; we < tiles.GetLength(1); we++)
+                {
+                    tiles[ns, we].update(elapsedMillis);
+                }
+            }
+        }
+
+        public virtual void draw(SpriteBatch spriteBatch)
+        {
+            for (int ns = 0; ns < tiles.GetLength(0); ns++)
+            {
+                for (int we = 0; we < tiles.GetLength(1); we++)
+                {
+                    tiles[ns, we].draw(TilePart.LEFTFACE, perspective, spriteBatch);
+                    tiles[ns, we].draw(TilePart.RIGHTFACE, perspective, spriteBatch);
+                    tiles[ns, we].draw(TilePart.SURFACE, perspective, spriteBatch);
+                    tiles[ns, we].draw(TilePart.STRUCTURE, perspective, spriteBatch);
+                }
+            }
+        }
+    }
+}
