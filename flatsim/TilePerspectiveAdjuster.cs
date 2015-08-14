6c087f7335113dc61a876363ed130b57dde6acb2
codyThompson <cody.thompson22@gmail.com>
2015-08-13 19:45:23 -0700

    Created the TilePerspectiveAdjuster interface which is the first step towards some sort of system for handling tile slopes.

---
+++ flatsim/TilePerspectiveAdjuster.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace flatsim
+{
+    public interface TilePerspectiveAdjuster
+    {
+        float getPriority();
+        void adjust(TileDrawInfo toAdjust, int coordNS, int coordWE, float height, TilePart part, string slope);
+    }
+}
