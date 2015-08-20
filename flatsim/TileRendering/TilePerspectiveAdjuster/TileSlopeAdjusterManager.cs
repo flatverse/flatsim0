4510e142a784ad28e96a61f16a452fd640c0ecec
codyThompson <cody.thompson22@gmail.com>
2015-08-19 20:23:43 -0700

    Created the TIleSlopeAdjusterManager which will eventually be used to determine the correct pixel position (and possible other things) based on a given slope.

---
+++ flatsim/TileRendering/TilePerspectiveAdjuster/TileSlopeAdjusterManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace flatsim
+{
+    public class TileSlopeAdjusterManager : TilePerspectiveAdjuster
+    {
+        public float priority = 0.1f;
+
+        private Dictionary<string, TilePerspectiveAdjuster> adjusters;
+
+        public TileSlopeAdjusterManager()
+        {
+            adjusters = new Dictionary<string, TilePerspectiveAdjuster>();
+        }
+
+        public virtual void init(TilePerspective perspective)
+        {
+            foreach (KeyValuePair<string, TilePerspectiveAdjuster> adjuster in adjusters)
+            {
+                adjuster.Value.init(perspective);
+            }
+        }
+
+        public virtual void add(string slope, TilePerspectiveAdjuster adjuster)
+        {
+            adjusters.Add(slope, adjuster);
+        }
+
+        public virtual void adjust(TileDrawInfo tileDrawInfo)
+        {
+            if (adjusters.ContainsKey(tileDrawInfo.slope))
+            {
+                adjusters[tileDrawInfo.slope].adjust(tileDrawInfo);
+            }
+        }
+
+        public virtual float getPriority()
+        {
+            return priority;
+        }
+    }
+}
