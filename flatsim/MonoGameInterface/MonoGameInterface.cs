18db6c7f6b67ada070aa7f4051830795b3ab23c0
codyThompson <cody.thompson22@gmail.com>
2015-08-19 20:02:39 -0700

    More file structure reorg.

---
+++ flatsim/MonoGameInterface/MonoGameInterface.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace flatsim
+{
+    public abstract class MonoGameInterface
+    {
+        public GraphicsDeviceManager graphics;
+        public SpriteBatch spriteBatch;
+        public ContentManager content;
+
+        public virtual void init(GraphicsDeviceManager graphics, SpriteBatch spriteBatch, ContentManager content)
+        {
+            this.graphics = graphics;
+            this.spriteBatch = spriteBatch;
+            this.content = content;
+        }
+
+        public abstract void loadContent();
+
+        public abstract void unloadContent();
+
+        public abstract void update(GameTime gameTime);
+
+        public abstract void draw(GameTime gameTime);
+    }
+
+    public interface MonoGameSetup
+    {
+        MonoGameInterface createGameInterface();
+    }
+}
