8a071d079686983853643940f70b94d5291f69a3
codyThompson <cody.thompson22@gmail.com>
2015-08-21 22:10:35 -0700

    Created new namespace for Angle45 slope implementation. Created skeleton for the TilePerspectiveAdjuster that will be used to adjust the pixel pos for height based on slope.

---
+++ flatsim/SlopeImpls/Angle45/SlopeAdjuster45.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+using flatsim;
+
+namespace flatsim.SlopeImpls.Angle45
+{
+    public class SlopeAdjuster45 : TilePerspectiveAdjuster
+    {
+        public float priority = 0.1f;
+        public TilePerspective perspective;
+
+        public SlopeAdjuster45()
+        {
+        }
+
+        public virtual void init(TilePerspective perspective)
+        {
+            this.perspective = perspective;
+        }
+
+        public virtual float getPriority()
+        {
+            return priority;
+        }
+
+        public virtual void adjust(TileDrawInfo drawInfo)
+        {
+            float newHeight = drawInfo.slopeInfo.getRelativeHeight(0, 0);
+            Vector2 newPos = perspective.getTilePixelPosition(drawInfo.coordNS, drawInfo.coordWE, newHeight, drawInfo.tilePart);
+            drawInfo.pos = newPos;
+        }
+    }
+}
